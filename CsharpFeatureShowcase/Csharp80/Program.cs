ec20930a7780197af1f9e5e905cb0e460b37d38d
Rick van Dam <rick-van-dam@outlook.com>
2018-12-18 16:43:37 +0100

    added csharp80 nullable example

---
+++ CsharpFeatureShowcase/Csharp80/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Csharp80
+{
+    class Program
+    {
+        #nullable enable
+        static void Main(string[] args)
+        {
+            NonNullableParameter(null); //Gives a warning because we are giving a null to a non nullable parameter
+            NullableParameter(null);
+
+            SomeClass? value = null;
+
+            NonNullableParameter(value); //Gives warning
+            if(value != null)
+            {
+                NonNullableParameter(value); //No warning
+            }
+        }
+
+        public static void NullableParameter(SomeClass? someclass) //Accepts a instance of SomeClass but the intent here its optional so it can be null
+        {
+
+        }
+
+        public static void NonNullableParameter(SomeClass someclass) //Requires a instance of SomeClass so it cannot be null
+        {
+
+        }
+
+        public class SomeClass
+        {
+
+        }
+    }
+}
