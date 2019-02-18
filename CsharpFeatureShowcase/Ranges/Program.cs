1dc656555352ee986d1673f57f57e41c692063ae
Rick van Dam <rick-van-dam@outlook.com>
2019-02-18 16:15:58 +0100

    added ranges example

---
+++ CsharpFeatureShowcase/Ranges/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemoryAndSpanNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someRange = 1..11;
+            var someText = "1234567890";
+            var substring = someText[1..^1];
+            Console.WriteLine(substring);
+            Console.ReadKey();
+        }
+    }
+}
